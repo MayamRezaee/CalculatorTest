d87f760ffc7421d9dfde37842d8c28a692d06098
Maryam Rezaee <maryam8203@yahoo.se>
2021-12-23 11:45:45 +0100

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using static System.Console;
+using System.Text;
+using System.Linq;
+
+namespace Calculator
+{
+    public class CalculatorProgram
+    {
+        public static double num1;
+        public static double num2;
+        public static double result = 0;
+
+
+        static void Main(string[] args)
+        {
+            bool isAlive = true;
+            WriteLine("\n");
+            ForegroundColor = ConsoleColor.Magenta;
+            WriteLine(String.Format("{0," + WindowWidth / 2 + "}", "*This is a calculator made by Maryam Rezaee* "));
+            ResetColor();
+
+            //This is the menu for the calculator.  
+            while (isAlive)
+            {
+                WriteLine("\n Please choose one of the operator below to calculate the result or enter e to exit the program" +
+                "\n \n +" +
+                "\n -" +
+                "\n *" +
+                "\n /" +
+                "\n e to exit the program \n");
+
+                //User selection for the desired operator.
+                //Operator menu will be Alive until user want to exit the program(by entering e)       
+                char operat = ReadLine()[0];
+                UserInput();
+                switch (operat)
+                {
+                    case '+':
+                        Clear();
+                        WriteLine("The result is :" +  Addition(num1, num2) + "\n\nPlease ENTER to coutiue");
+                        
+                        break;
+
+                    case '-':
+                        Clear();
+                        WriteLine("The result is :" + Subtraction(num1, num2) + "\n\nPlease ENTER to coutiue");
+
+                        break;
+                    case '*':
+                        Clear();
+                        WriteLine("The result is :" + Multiplication(num1, num2) + "\n\nPlease ENTER to coutiue");
+                        break;
+                    case '/':
+                        Clear();
+                        WriteLine("The result is :" + Division(num1, num2) + "\n\nPlease ENTER to coutiue");
+                        break;
+                    case 'e':
+                        Clear();
+                        ForegroundColor = ConsoleColor.Magenta;
+                        WriteLine("\n Thank you for tryig my project");
+                        ResetColor();
+                        isAlive = false;
+                        Environment.Exit(0);
+
+                        break;
+                    default:
+                        WriteLine(" {0} is not a valid selection, please try again", operat);
+                        break;
+
+                }
+                //WriteLine("\n The result is {0} ", result,  "\n Please ENTER to continu");
+                ReadKey();
+                Clear();
+            }
+
+
+        }
+        //This method will take the user's inputs, which will be used later in the program, for calculation. 
+        public static void UserInput()
+        {
+            WriteLine("Please enter your first number :");
+            num1 = Double.Parse(ReadLine());
+            WriteLine("Please enter your second number :");
+            num2 = Double.Parse(ReadLine());
+        }
+
+        //Addition of the two numbers entered by user.
+        public static double Addition(double num1, double num2)
+        {
+            double result = num1 + num2;
+            return result;
+        }
+
+        //overloading Addition method with parsing an array as parameter
+        public static double Addition(double[] nums)
+        {
+            double result = 0; 
+            for ( int i = 0; i < nums.Length; i++)
+            {
+                result += nums[1];
+            }
+            return result;
+        }
+
+        //Addition of the two numbers entered by user.
+        public static double Subtraction(double num1, double num2)
+        {
+            double result = num1 - num2;
+            return result;
+        }
+        //overloading Subtraction method with parsing an array as parameter
+        public static double Subtraction(double[] nums)
+        {
+            double result = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result -= nums[1];
+            }
+            return result;
+        }
+
+        //Multiplication of the two numbers entered by user.
+        public static double Multiplication(double num1, double num2)
+        {
+            double result = num1 * num2;
+            return result;
+        }
+
+        //Division of the two numbers entered by user.
+        public static double Division(double num1, double num2)
+        {
+            double result = num1 / num2;
+            try
+            {
+               
+
+                if (Double.IsInfinity(result))
+                {
+                    throw new ArgumentException("Division by zero is not allowed, please try again.");
+                }
+              // return result;
+ 
+            }
+
+            catch (ArgumentException e)
+            {
+                WriteLine(e.Message);
+            }
+            
+            return result;
+
+        }
+         
+
+
+        }
+    }
---
+++ CalculatorTest/UnitTest1.cs
@@ -0,0 +1,63 @@
+using System;
+using Xunit;
+using Calculator;
+
+namespace CalculatorTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestAddition()
+        {
+            double num1 = 20;
+            double num2 = 5;
+            double expectedResult = 25;
+            double result = CalculatorProgram.Addition(num1, num2);
+            Assert.Equal(expectedResult, result);
+        }
+        [Fact]
+
+        public void TestSubtraction()
+        {
+            double num1 = 20;
+            double num2 = 5;
+            double expectedResult = 15;
+            double result = CalculatorProgram.Subtraction(num1, num2);
+            Assert.Equal(expectedResult, result);
+        }
+        [Fact]
+        public void TestMultiplication()
+        {
+            double num1 = 20;
+            double num2 = 5;
+            double expectedResult = 100;
+            double result = CalculatorProgram.Multiplication(num1, num2);
+            Assert.Equal(expectedResult, result);
+        }
+        
+        [Theory]
+        [InlineData(4, 2, 2)]
+        [InlineData(9, 2, 4.5)]
+        [InlineData(5, 3, 1.67)]
+        public void TestDivisionTheory(double num1, double num2, double expectedResult)
+        {
+         
+            double result = CalculatorProgram.Division(num1, num2);
+            Assert.Equal(expectedResult, result,2);
+        }
+        [Fact]
+        public void TestDivideByZeroException()
+        {
+            double num1 = 2;
+            double num2 = 0;
+            var expectedResult = Assert.Throws<DivideByZeroException> (() => CalculatorProgram.Division(num1, num2));
+            
+            Assert.Equal("Can not be divided by zero", expectedResult.Message);
+        }
+
+
+
+    }
+
+
+}
