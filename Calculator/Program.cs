af6ade7ac4ef0f69b86f5d45deb899579289dad1
Maryam Rezaee <93116377+MayamRezaee@users.noreply.github.com>
2021-12-23 13:35:06 +0100

    Update Program.cs
    for loop problem fixed

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -98,7 +98,7 @@ namespace Calculator
             double result = 0; 
             for ( int i = 0; i < nums.Length; i++)
             {
-                result += nums[1];
+                result += nums[i];
             }
             return result;
         }
@@ -115,7 +115,7 @@ namespace Calculator
             double result = 0;
             for (int i = 0; i < nums.Length; i++)
             {
-                result -= nums[1];
+                result -= nums[i];
             }
             return result;
         }
